68c0caa394cf0f047aa0dccd347dfa295338e5da
Geena Jackson <gjacks303@gmail.com>
2022-04-26 09:27:43 -0600

    Set up repo and fetch urls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,15 @@
-export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+
+const fetchAPI = {
+  getAllUrls() {
+    return fetch('http://localhost:3001/api/v1/urls')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Cannot load ideas!')
+      } else {
+        return response.json()
+      }
+    })
+  }
 }
+
+export default fetchAPI
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { getUrls } from '../../apiCalls';
+import fetchAPI from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      urls: []
-    }
+const App = () => {
+  const [urls, setUrls] = useState([])
+
+  const fetchData = () => {
+    fetchAPI.getAllUrls()
+    .then(data => setUrls(data.urls))
   }
 
-  componentDidMount() {
-  }
+  useEffect(() => {
+    fetchData()
+  }, [])
 
-  render() {
     return (
       <main className="App">
         <header>
@@ -23,10 +23,9 @@ export class App extends Component {
           <UrlForm />
         </header>
 
-        <UrlContainer urls={this.state.urls}/>
+        <UrlContainer urls={urls}/>
       </main>
-    );
-  }
+    )
 }
 
 export default App;
